0538edd98037470c765382c8d6b8b4828f239e85
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2022-12-09 01:08:46 +0900

    change port to capital case

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
 
-  const port = config.get<number>('port', 3000);
+  const port = config.get<number>('PORT', 3000);
   await app.listen(port);
 
   Logger.log(`Listening on port ${port}`, 'Bootstrap');
