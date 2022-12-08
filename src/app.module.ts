6db0553c7836e8becffff11b7aa57b2bd2c5f80e
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2022-12-09 00:59:40 +0900

    add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppGateway } from './app.gateway';
+import { ConfigModule } from '@nestjs/config/dist';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     InvokeModule.register({
       servers: [
         {
